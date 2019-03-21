53f94bc4a44e50ed52452456464b1d5c7d8ff333
bk2299 <bk2299@att.com>
2019-03-20 17:55:28 -0700

    Updated files to refer 'drawpanda'

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# homebrew-testfib
-Testing the Go func with Homebrew
+# homebrew-drawpanda
+
+Simple Ruby Formula to demo Homebrew. 
+'_drawpanda.rb_' is the formula that calls a script to Draw Panda in CLI.
+
+To run it in your local, try
+
+`brew tap bhaskar-reddy/drawpanda`
+
+followed by 
+
+`brew install drawpanda`
+
+Once this is done, you can run the command
+
+`drawpanda`
+
+and the script is called to execute in your local.
---
+++ drawpanda.rb
@@ -0,0 +1,17 @@
+class Drawpanda < Formula
+  desc "Executing a Script to Draw Panda"
+  homepage "https://github.com/bhaskar-reddy/homebrew-drawpanda"
+
+  url "https://github.com/bhaskar-reddy/homebrew-drawpanda/raw/master/drawart-1.0.0.tar.gz"
+  sha256 "2836ccac9071cf5994d5977cb043219258cfeaa03739c45b03b27a8801c1ed20"
+  head "https://github.com/bhaskar-reddy/homebrew-drawpanda.git"
+  version "1.0.0"
+
+  depends_on "curl"
+
+  bottle :unneeded
+
+  def install
+    bin.install "drawpanda"
+  end
+end
