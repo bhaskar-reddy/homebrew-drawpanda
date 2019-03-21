91aad7904ca5e27bf756a290cfb6a93a5543b3d3
bk2299 <bk2299@att.com>
2019-03-20 17:15:30 -0700

    Updated SHA256

--- testfib.rb
+++ testfib.rb
@@ -3,7 +3,7 @@ class Testfib < Formula
   homepage "https://github.com/bhaskar-reddy/homebrew-testfib"
 
   url "https://github.com/bhaskar-reddy/homebrew-testfib/blob/master/drawart-1.0.0.tar.gz"
-  sha256 "47bc7b452511a98e9069718d5fad253c0d02cab60e433e7696092757fdacad80"
+  sha256 "f62fa9fe08fd8797bbe46176f1d792d6bb2907941383d2c3c96635479ef6fd6d"
   head "https://github.com/bhaskar-reddy/homebrew-testfib.git"
   version "1.0.0"
 
