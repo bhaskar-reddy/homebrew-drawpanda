d92c1bdfe7e3b0ffa2b66a1cb03330d3123b3089
bk2299 <bk2299@att.com>
2019-03-20 17:19:55 -0700

    Updated URL

--- testfib.rb
+++ testfib.rb
@@ -2,7 +2,7 @@ class Testfib < Formula
   desc "Executing a test script"
   homepage "https://github.com/bhaskar-reddy/homebrew-testfib"
 
-  url "https://github.com/bhaskar-reddy/homebrew-testfib/blob/master/drawart-1.0.0.tar.gz"
+  url "https://github.com/bhaskar-reddy/homebrew-testfib/raw/master/drawart-1.0.0.tar.gz"
   sha256 "f62fa9fe08fd8797bbe46176f1d792d6bb2907941383d2c3c96635479ef6fd6d"
   head "https://github.com/bhaskar-reddy/homebrew-testfib.git"
   version "1.0.0"
